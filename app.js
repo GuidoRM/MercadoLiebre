ce4073ebc1613ca41e6eb824eba021715520cde3
GuidoRM <guidorafaelmamani100@gmail.com>
2023-02-08 14:30:45 -0300

    Subiendo página

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+const path = require('path')
+
+
+app.listen(port, () => {
+    console.log("Servidor funcionando en puerto localhost:"+port)
+});
+
+app.use(express.static("public"));
+
+app.get("/",(req,res) => {
+    res.sendFile(__dirname+"/views/home.html");
+});
+app.get("/register",(req,res) => {
+    res.sendFile(__dirname+"/views/register.html");
+});
+
+app.get("/login",(req,res) => {
+    res.sendFile(__dirname+"/views/login.html");
+});
